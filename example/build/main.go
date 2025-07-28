6b66619c3fff476424639e4981b253e7649c7fa9
sam <aquaticcalf@proton.me>
2025-07-28 14:20:29 +0000

    build to html

---
+++ example/build/main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"log"
+	"os"
+
+	. "github.com/aquaticcalf/tago"
+)
+
+func main() {
+	page := Html(
+		Head(
+			Tailwind(),
+			Title("tago"),
+			Link(
+				Attr("rel", "shortcut icon"),
+				Attr("href", "https://aqclf.xyz/favicon.png"),
+				Attr("type", "image/png"),
+			),
+			Meta(
+				Attr("name", "viewport"),
+				Attr("content", "width=device-width, initial-scale=1.0"),
+			),
+		),
+		Body(
+			Class("bg-black text-white min-h-[100dvh] flex flex-col items-center justify-center"),
+			H1(Class("text-4xl"), "Hello, world!"),
+		),
+	)
+
+	f, err := os.Create("index.html")
+	if err != nil {
+		log.Fatalf("failed to create file : %v", err)
+	}
+	defer f.Close()
+
+	if err := page.Render(f); err != nil {
+		log.Fatalf("failed to render HTML : %v", err)
+	}
+}
